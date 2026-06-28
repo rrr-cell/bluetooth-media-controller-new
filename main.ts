dea363baff395607f8636fab3b47fe0ca6dca230
Aayansh Athaghara <aayansh.ath@gmail.com>
2026-06-28 11:10:21 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,18 +6,69 @@ bluetooth.onBluetoothDisconnected(function () {
 })
 input.onGesture(Gesture.LogoUp, function () {
     media.sendCode(media.keys(media._MediaKey.vol_up))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     media.sendCode(media.keys(media._MediaKey.previous))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
+})
+input.onButtonPressed(Button.AB, function () {
+    media.sendCode(media.keys(media._MediaKey.stop))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
 input.onGesture(Gesture.Shake, function () {
     media.sendCode(media.keys(media._MediaKey.playPause))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
 input.onGesture(Gesture.TiltRight, function () {
     media.sendCode(media.keys(media._MediaKey.next))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
 input.onGesture(Gesture.LogoDown, function () {
     media.sendCode(media.keys(media._MediaKey.vol_down))
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
 })
 basic.forever(function () {
     media.startMediaService()
