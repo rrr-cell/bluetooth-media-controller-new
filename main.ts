059a4a29f6466018564988fa0795f425e891f9a6
Aayansh Athaghara <aayansh.ath@gmail.com>
2026-06-28 11:22:10 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,69 +7,76 @@ bluetooth.onBluetoothDisconnected(function () {
 input.onGesture(Gesture.LogoUp, function () {
     media.sendCode(media.keys(media._MediaKey.vol_up))
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    media.sendCode(media.keys(media._MediaKey.previous))
+    media.sendCode(media.keys(media._MediaKey.next))
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        # # . . #
+        # . # . #
+        # . . # #
+        # . # . #
+        # # . . #
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     media.sendCode(media.keys(media._MediaKey.stop))
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
     media.sendCode(media.keys(media._MediaKey.playPause))
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        . # . . .
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.TiltRight, function () {
-    media.sendCode(media.keys(media._MediaKey.next))
+    media.sendCode(media.keys(media._MediaKey.previous))
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
+        # . . # #
+        # . # . #
+        # # . . #
+        # . # . #
+        # . . # #
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
 input.onGesture(Gesture.LogoDown, function () {
     media.sendCode(media.keys(media._MediaKey.vol_down))
     basic.showLeds(`
         . . . . .
         . . . . .
-        . . . . .
+        # # # # #
         . . . . .
         . . . . .
         `)
     basic.pause(1000)
+    basic.clearScreen()
 })
+basic.showString("BLUETOOTH PARING")
 basic.forever(function () {
     media.startMediaService()
 })
